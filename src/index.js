d66c624a8a9e4108c6e2bfb899408580eebb3501
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-08-24 19:24:28 -0400

    commit tree d900ae7c602666a19beeb9728b97a82b8dca5fec

--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadImages();
+    loadBreeds();
+  });
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+    fetch(imgUrl)
+    .then(response=> response.json())
+    .then(result => {
+      result.message.forEach(image => addImage(image))
+    });
+} 
+
+function addImage(picUrl) {
+    let container = document.querySelector('dog-image-container');
+    let imgElement = document.createElement('img');
+    imgElement.src = picUrl;
+    container.appendChild(imgElement);
+}
+
+function loadBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+    fetch(breedUrl)
+    .then(response=> response.json())
+    .then(result => {
+      breeds = Object.keys(result.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    });
+}
+
+function updateBreedList(breeds)
+let ul = document.querySelector('#dog-breeds');
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
+
+function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function (event) {
+      selectBreedsStartingWith(event.target.value);
+    });
+}
+
+function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+  }
+
+function removeChildren(element) {
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+}
+
+function addBreed(breed) {
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    li.innerText = breed;
+    li.style.cursor = 'pointer';
+    ul.appendChild(li);
+    li.addEventListener('click', updateColor);
+  }
+  
+  function updateColor(event) {
+    event.target.style.color = 'pink';
+  }
